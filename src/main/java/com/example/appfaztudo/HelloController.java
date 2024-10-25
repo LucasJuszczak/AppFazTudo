b9fc4e4a7ee0f05fbc415536a588713e86ff5c99
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2024-10-25 20:07:00 -0300

    App Update

--- src/main/java/com/example/appfaztudo/HelloController.java
+++ src/main/java/com/example/appfaztudo/HelloController.java
@@ -1,5 +1,27 @@
 package com.example.appfaztudo;
 
+import javafx.fxml.FXML;
+
 public class HelloController {
+
+    @FXML
+    protected void onCalcularIMCButtonClick(){
+
+    }
+
+    @FXML
+    protected void onConversaoTemperaturaButtonClick(){
+
+    }
+
+    @FXML
+    protected void onDoacoesButtonClick(){
+
+    }
+
+    @FXML
+    protected void onSairButtonClick(){
+        System.exit(0);
+    }
 
 }
